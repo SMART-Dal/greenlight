eb08a1e23af0a592659e0ac5c52103cb0924b1b7
Saurabhsingh Rajput <saurabh@dal.ca>
2023-10-06 14:04:32 -0300

    extra files

--- codegreen/main.py
+++ codegreen/main.py
@@ -5,28 +5,35 @@ from rich.markdown import Markdown
 
 app = typer.Typer(rich_markup_mode="rich",help="[green]🍃 CodeGreen: Your Passport to a Greener Code! 🍃[/green]")
 
-
 @app.callback()
 def callback():
     """
-    Awesome Portal Gun
+    CodeGreen
     """
 
 
 @app.command()
-def shoot():
+def setup():
     """
-    Shoot the portal gun
+    This command will calculate energy consumption for a single given github project
+    """
+    print("Shooting portal gun")
+
+@app.command()
+def start_measurement():
+    """
+    This command will start the energy measurement server
     """
     print("Shooting portal gun")
 
 
-@app.command()
-def load():
-    """
-    Load the portal gun
-    """
-    print("Loading portal gun")
+# @app.command()
+# def load():
+#     """
+#     Load the portal gun
+#     """
+#     print("Loading portal gun")
 
+# only use this by uncommenting for testing purposes
 if __name__ == "__main__":
     app()
