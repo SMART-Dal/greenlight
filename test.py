580dfa022aa5d3051858ae0b56cd3c5f7ccdf5aa
Saurabhsingh Rajput <saurabh@dal.ca>
2023-12-08 21:19:25 -0400

    Add codegreen and pympler to requirements.txt

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-from pathlib import Path
-
-script = "/home/saurabh/method-energy-dataset/projects/tensorflow_docs_patched/site/en/tutorials/audio/transfer_learning_audio_method-level.py"
-base_path = os.path.basename(Path(script)).replace("_method-level.py","")
-# base_path, ext = os.path.splitext(Path(script))
-print(base_path)
